5236afc8ad5ca0d784f1bfbbb0d06b13b8034a0e
mohamed <mohamedboukab202@gmail.com>
2025-03-19 10:44:45 +0000

    Added authentication system

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\LoginRequest;
+use App\Http\Requests\RegisterRequest;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+
+class UserController extends Controller
+{
+    public function register(RegisterRequest $request){
+
+        $request->validated();
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password) 
+        ]);
+        
+        return response()->json(['message' => 'User Registred successfully','user' => $user], 201);
+    }
+
+
+    public function login(LoginRequest $request){
+
+        $request->validated();
+        
+        if(!Auth::attempt($request->only('email','password'))){
+            return response()->json(['message' => 'Email or Password incorrect'], 401);
+        }
+
+        $user = User::where('email', $request->email)->FirstOrFail();
+        $token = $user->createToken('auth_Token')->plainTextToken;
+
+        return response()->json(['message' => 'You Logged In successfully', 'user' => $user, 'Token' => $token], 201);
+    }
+
+
+    public function logout(Request $request){
+       $request->user()->currentAccessToken()->delete();
+       return response()->json(['message' => 'You Logged out successfully']);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,17 +1,18 @@
 <?php
 
 use App\Http\Controllers\QuoteController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
-Route::apiResource('/quotes',QuoteController::class);
-Route::get('/quotes/random/{quote}', [QuoteController::class, 'randomQuote']);
-Route::get('/quotes/popular/{quote}', [QuoteController::class, 'getPopularQuotes']);
-Route::get('/quotes/filter/{quote}', [QuoteController::class, 'filterByWord']);
+use App\Http\Controllers\UserController;
 
 
+Route::middleware('auth:sanctum')->group(function () {
+   Route::apiResource('/quotes',QuoteController::class);
+   Route::get('/quotes/random/{quote}', [QuoteController::class, 'randomQuote']);
+   Route::get('/quotes/popular/{quote}', [QuoteController::class, 'getPopularQuotes']);
+   Route::get('/quotes/filter/{quote}', [QuoteController::class, 'filterByWord']);
+});
+
+Route::post('/register',[UserController::class,'register']);
+Route::post('/login',[UserController::class,'login']);
+Route::post('/logout',[UserController::class,'logout'])->middleware('auth:sanctum');;
 
